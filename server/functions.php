c87ee534e2f22844c9b2ed3d1ecac6cf7c788fac
atharvadeosthale <atharva.deosthale17@gmail.com>
2019-08-20 09:42:33 -0700

    Update functions.php

--- server/functions.php
+++ server/functions.php
@@ -1,5 +1,6 @@
 <?php
 
+// This function reads the data, encodes it in JSON format and prints it (send it as response)
 function send_response($code,$message) {
 	$response["status_code"] = $code;
 	$response["status_message"] = $message;
