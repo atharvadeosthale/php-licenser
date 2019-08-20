34dd12e235f3d3a48e4612f89ad530903288c373
atharvadeosthale <atharva.deosthale17@gmail.com>
2019-08-20 09:41:25 -0700

    Update server.php

--- server/server.php
+++ server/server.php
@@ -1,7 +1,10 @@
 <?php
 
+// Including the Database and the Function files
 require_once "database.php";
-require_once "functions.php";
+require_once "functions.php"; 
+
+// Tell the Client that the response received is of JSON type
 header("Content-Type: application/json");
 
 if(!isset($_POST["license_key"]))
@@ -24,11 +27,13 @@ if(!isset($_POST["domain_name"]))
 }
 
 $domain_name = $_POST["domain_name"];
-
+ 
+// SQL Query to validate the License provided by the client
 $sql = "SELECT * FROM license_keys WHERE license_key='$license_key' AND package_name='$package_name' AND domain_name='$domain_name';";
 $result = mysqli_query($conn, $sql);
 $count = mysqli_num_rows($result);
 
+// Authenticate and send response accordingly
 if($count == 1)
 {
 	send_response("200","Authorised");
@@ -38,4 +43,5 @@ else
 	send_response("403","Unauthorised");
 }
 
+// End of file
 ?>
