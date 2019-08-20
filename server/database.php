732d1954405fe1d798a18d97a071cb64df326ed5
atharvadeosthale <atharva.deosthale17@gmail.com>
2019-08-20 09:44:44 -0700

    Update database.php

--- server/database.php
+++ server/database.php
@@ -1,10 +1,17 @@
 <?php
 
+// PHP Licenser Script
+// Made by Atharva Deosthale
+
+// This file contains Database details and creates a connection to it
+// Use of mysqli is done instead of PDO or mysql
+// Making a little changes, this program can work on mysql and PDO
 $db["host"] = "localhost";
 $db["user"] = "root";
 $db["pass"] = "";
 $db["name"] = "licenser";
 
+// Initiate Connection
 $conn = mysqli_connect($db["host"],$db["user"],$db["pass"],$db["name"]);
 
 if(!$conn)
